97bf0a5966747e4497cff1d6b3cac52336bc8394
bboyhangqi <forworkhq@163.com>
2018-04-18 12:39:16 -0500

    commit

--- app/src/main/java/app/mumandroidproject/ui/activity/PreviewActivity.kt
+++ app/src/main/java/app/mumandroidproject/ui/activity/PreviewActivity.kt
@@ -2,12 +2,16 @@ package app.mumandroidproject.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import app.mumandroidproject.R
 
 class PreviewActivity : AppCompatActivity() {
 
+    private val TAG = "PreviewActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
+        Log.d(TAG,"url: "+intent.getStringExtra("url"))
     }
 }
--- app/src/main/java/app/mumandroidproject/ui/adpter/ColumnAdapter.kt
+++ app/src/main/java/app/mumandroidproject/ui/adpter/ColumnAdapter.kt
@@ -1,6 +1,7 @@
 package app.mumandroidproject.ui.adpter
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
 import app.mumandroidproject.bean.WallpaperItem
@@ -11,6 +12,7 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import app.mumandroidproject.R
 import app.mumandroidproject.extension.loadByGlide
+import app.mumandroidproject.ui.activity.PreviewActivity
 
 
 /**
@@ -25,6 +27,17 @@ class ColumnAdapter(var data: List<WallpaperItem>, var categoryName: String, win
 
     var headItem: WallpaperItem? = null
     var itemList = mutableListOf<Array<WallpaperItem>>()
+
+    init {
+        val display = windowManager.getDefaultDisplay()
+        val size = Point()
+        display.getSize(size)
+        screenWidth = size.x
+        screenHeight = size.y
+        headItem = data[0]
+        data.filterIndexed { index, wallpaperItem -> index != 0 }
+                .forEachIndexed { index, wallpaperItem -> if (index % 2 == 0) itemList.add(arrayOf(data[index], data[index + 1])) }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.wallpaper_item, parent, false)
@@ -47,6 +60,7 @@ class ColumnAdapter(var data: List<WallpaperItem>, var categoryName: String, win
             holder.tvHeadCount.text = data.size.toString()
             holder.iv2.visibility = View.GONE
             holder.iv1.visibility = View.GONE
+            holder.itemHead = headItem
         } else {
             holder.headLayout.visibility = View.GONE
             holder.iv2.visibility = View.VISIBLE
@@ -58,11 +72,18 @@ class ColumnAdapter(var data: List<WallpaperItem>, var categoryName: String, win
             holder.iv2.setPadding(2, 2, 0, 2)
             holder.iv1.loadByGlide(itemList[position][0].url)
             holder.iv2.loadByGlide(itemList[position][1].url)
+            holder.itemForIv1 = itemList[position][0]
+            holder.itemForIv2 = itemList[position][1]
         }
 
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        var itemHead: WallpaperItem? = null
+        var itemForIv1: WallpaperItem? = null
+        var itemForIv2: WallpaperItem? = null
+
         var iv1 = itemView.findViewById<ImageView>(R.id.iv1)
         var iv2 = itemView.findViewById<ImageView>(R.id.iv2)
 
@@ -72,19 +93,27 @@ class ColumnAdapter(var data: List<WallpaperItem>, var categoryName: String, win
         var tvHeadCatelog = itemView.findViewById<TextView>(R.id.tv_head_catelog)
         var tvHeadCount = itemView.findViewById<TextView>(R.id.tv_head_count)
 
+        init {
+            iv1.setOnClickListener(this)
+            iv2.setOnClickListener(this)
+            headLayout.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            when (v?.id) {
+                R.id.iv1 -> goToPreview(itemForIv1?.url)
+                R.id.iv2 -> goToPreview(itemForIv2?.url)
+                R.id.rl_head -> goToPreview(itemHead?.url)
+            }
+        }
+
+        private fun goToPreview(url: String?) {
+            val intent = Intent(itemView.context, PreviewActivity::class.java)
+            intent.putExtra("url", url)
+            itemView.context.startActivity(intent)
+        }
     }
 
-
-    init {
-        val display = windowManager.getDefaultDisplay()
-        val size = Point()
-        display.getSize(size)
-        screenWidth = size.x
-        screenHeight = size.y
-        headItem = data[0]
-        data.filterIndexed { index, wallpaperItem -> index != 0 }
-                .forEachIndexed { index, wallpaperItem -> if (index % 2 == 0) itemList.add(arrayOf(data[index], data[index + 1])) }
-    }
 
 
 }
